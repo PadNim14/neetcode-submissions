14e94092ad4506a112cc9807390538c314c0cadb
Nimal Padmanabhan <65453164+PadNim14@users.noreply.github.com>
2026-07-20 19:22:24 -0700

    Bulk sync: 25 submissions

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        hMap = {}
+        res = []
+
+        for num in nums:
+            print(num)
+            if num in hMap:
+                hMap[num] += 1
+            else:
+                hMap[num] = 1
+        
+        # print(hMap)
+
+        sortedFreq = dict(sorted(hMap.items(), key=lambda item: item[1], reverse=True))
+        # print(sortedFreq)
+        for val in sortedFreq:
+            res.append(val)
+            if len(res) == k:
+                return res
+    
+        
---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # naive solution
+        # time O(n^2) space O(1)
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # return []
+
+
+        # optimal solution with dictionary
+
+        diffDict = {}
+        res = []
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in diffDict:
+                return [diffDict[diff], i]
+            diffDict[num] = i
